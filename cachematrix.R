fc72586c1163ad7b08d4dc104bd27f50eb6a89ac
Benjamin Alibuyog <balibuyog@gmail.com>
2025-02-23 01:19:40 +1100

    Assignment submission - Benjamin Alibuyog

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Benjamin comment - this function creates a matrix object
+## that can store a matrix and its inverse
 
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL # Benjamin comment - initialises the cache for the inverse
+  set <- function(y) {
+    x <<- y # Benjamin comment - stores the new matrix
+    inv <<- NULL # Benjamin comment - resets cached inverse
+  }
+  get <- function() x # Benjamin comment - retrieves matrix
+  setInverse <- function(inverse) inv <<- inverse #Benjamin comment - stores inverse in cache
+  getInverse <- function() inv # Benjamin comment - retrieve cached inverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse, # Benjamin comment - allows user to update matrix
+       getInverse = getInverse) # Benjamin comment - returns the stored matrix
 }
 
 
 ## Write a short comment describing this function
 
+## Benjamin comment - computes the inverse of the matrix
+## if inverse exists, it will retrieve the cache
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse() # Benjamin comment - retrieve cached inverse
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  
+  matx <- x$get() #Benjamin comment - retrieves a matrix
+  inv <- solve(matx, ...) #Benjamin comment - computes inverse
+  x$setInverse(inv) #Benjamin comment - cache inverse
+  inv
+  ## Return a matrix that is the inverse of 'x'
 }
+
